e3e11504310a0755cbb454d60525bc26f9016222
MaxSpacer <max_spacer@mail.ru>
2019-09-09 15:37:20 +0300

    initial

---
+++ bsh_test_smena/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bsh_test_smena project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bsh_test_smena.settings')
+
+application = get_wsgi_application()
